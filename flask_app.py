f768d8fc1d5c8993f79f1cdbfcaeac1c57cf0c44
theusjf <matheusdejesusf@gmail.com>
2024-10-23 14:40:01 +0000

    Commit 1

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -8,88 +8,25 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 @app.route('/')
-def home():
-    current_time = datetime.utcnow()
-    html_template = '''
-    {% extends "bootstrap/base.html" %}
+def index():
+     return render_template('index.html', current_time=datetime.utcnow())
 
-    {% block title %}Flasky{% endblock %}
+@app.route('/user/<name>/<pt>/<inst>')
+def user(name, pt, inst):
+    return render_template('user.html', name=name, pt=pt, inst=inst)
 
-    {% block navbar %}
-    <div class="navbar navbar-inverse" role="navigation">
-        <div class="container">
-            <a class="navbar-brand" href="/">Flasky</a>
-            <a class="navbar-brand" href="/">Home</a>
-        </div>
-    </div>
-    {% endblock %}
-
-    {% block content %}
-    <div class="container">
-        <h1>Hello World!</h1>
-        <hr>
-        <p>The local date and time is {{ moment(current_time).format('LLL') }}.</p>
-        <p>That was {{ moment(current_time).fromNow(refresh=True) }}.</p>
-    </div>
-    {% endblock %}
-
-    {% block scripts %}
-    {{ super() }}
-    {{ moment.include_moment() }}
-    {% endblock %}
-    '''
-    return render_template_string(html_template, current_time=current_time)
-
-@app.route('/user/<name>')
-def identificacao(name):
-
-    html_template = '''
-    {% extends "bootstrap/base.html" %}
-
-    {% block title %}Ident{% endblock %}
-
-    {% block navbar %}
-    <div class="navbar navbar-inverse" role="navigation">
-        <div class="container">
-            <a class="navbar-brand" href="/">Flasky</a>
-            <a class="navbar-brand" href="/">Home</a>
-        </div>
-    </div>
-    {% endblock %}
-
-    {% block content %}
-    <div class="container">
-        <h1>Hello, {{ name }}!</h1>
-        <hr>
-    </div>
-    {% endblock %}
-    '''
-
-    return render_template_string(html_template, name=name)
+@app.route('/contextorequisicao/<name>')
+def req(name):
+    user_agent = request.headers.get('User-Agent')
+    remote_addr = request.remote_addr
+    host = request.host
+    return render_template('contextorequisicao.html', name=name, user_agent=user_agent, remote_addr=remote_addr, host=host)
 
 @app.errorhandler(404)
 def page_not_found(e):
+    return render_template('404.html'), 404
 
-    html_template = '''
-    {% extends "bootstrap/base.html" %}
 
-    {% block title %}404{% endblock %}
-
-    {% block navbar %}
-    <div class="navbar navbar-inverse" role="navigation">
-        <div class="container">
-            <a class="navbar-brand" href="/">Flasky</a>
-            <a class="navbar-brand" href="/">Home</a>
-        </div>
-    </div>
-    {% endblock %}
-
-    {% block content %}
-    <div class="container">
-        <h1>Not Found</h1>
-        <hr>
-    </div>
-    {% endblock %}
-    '''
-
-    return render_template_string(html_template), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
